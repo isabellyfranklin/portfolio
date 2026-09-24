dfc7d2c3bdae6bb382986d1df06f025029f9ad45
Isabelly Franklin <isafranklin883@gmail.com>
2026-09-24 01:48:55 -0300

    Hero quase finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Hero } from './components/Hero/Hero'
 import { Nav } from './components/Nav/Nav'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
   return (
     <>
       <Nav/>
-      
+      <Hero/>
     </>
   )
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,22 @@
-import style from './Hero.module.css'
+import style from "./Hero.module.css";
 
 export function Hero() {
+  return (
+    <section className={style.hero}>
+      <span className={style.portfolio}>PORTFÓLIO</span>
+      <h1>Olá, eu sou</h1>
+      <h2 className={style.name}>Isabelly Franklin</h2>
 
-    return(
-        <section className={style.hero}>
-            <span className={style.portfolio}>PORTFÓLIO</span>
-            <h1>Olá, eu sou</h1>
-            <h2>Isabelly Franklin</h2>
-        </section>
-    )
+      <p className={style.description}>
+        <span>
+          Desenvolvedora Front-End em formação, focada em criar interfaces que
+          unem estética e funcionalidade.
+        </span>
+      </p>
+
+      <a href="#projetos" className={style.button}>
+        Ver Projetos ↗
+      </a>
+    </section>
+  );
 }
